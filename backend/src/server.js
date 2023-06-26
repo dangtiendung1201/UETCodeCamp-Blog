448cc39c9762a895847674a80f6b37c326ca6599
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-06-26 18:02:27 +0700

    Add: Setting up server

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+console.log(PORT);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URL);
+console.log("Database connected!");
+
+// routing
+app.get("/", (req, res) => {
+    res.json({ success: true, message: "Welcome to express" }).status(200);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
