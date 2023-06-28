a47ba664364673fb8ecbf5f602d251d530275c6c
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-06-29 01:56:50 +0700

    Add: Search button

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -13,35 +13,57 @@ export const getAllPosts = (req, res) => {
 };
 
 export const searchPost = (req, res) => {
+    const locals = {
+        title: "Search",
+        description: "Blog"
+    }
     const { title } = req.query;
 
     if (!title) {
         return res.json({ success: false, message: "Please fill all the fields" }).status(400);
     }
 
-    Post.find({ title: { $regex: title, $options: "i" } })
+    const data = Post.find({ title: { $regex: title, $options: "i" } })
         .then(posts => {
             return res.json({ success: true, posts }).status(200);
         })
         .catch(err => {
             console.log(err);
         });
+
+    res.render("search", {
+        data,
+        locals,
+        currentRoute: '/'
+    });
 };
 
 export const getPostById = (req, res) => {
+    const locals = {
+        title: "Search",
+        description: "Blog"
+    }
+
     const { id } = req.params;
+    let slug = req.params.id;
 
     if (!id) {
         return res.json({ success: false, message: "Please fill all the fields" }).status(400);
     }
 
-    Post.findOne({ id })
+    const data = Post.findOne({ id })
         .then(post => {
             return res.json({ success: true, post }).status(200);
         })
         .catch(err => {
             console.log(err);
         });
+
+    res.render("post", {
+        data,
+        locals,
+        currentRoute: '/posts/${slug}'
+    });
 };
 
 export const createPost = (req, res) => {
