6156be4ae808cd9833cf3956eea6b008ff7a2153
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-07-03 17:30:56 +0700

    add post

--- api/routes/addRoute.js
+++ api/routes/addRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addAdmin } from "../controllers/addController";
+import { addInterface, addAdmin } from "../controllers/addController";
 
 const addRoute = Router();
 
+addRoute.get("/", addInterface);
 addRoute.post("/", addAdmin);
 
 export default addRoute;
