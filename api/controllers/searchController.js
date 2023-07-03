f724498a2c290de00b076cfe7de24e275a59d3e9
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-07-03 10:55:44 +0700

    Fix: Minor bug at render posts

--- api/controllers/searchController.js
+++ api/controllers/searchController.js
@@ -37,7 +37,7 @@ export const getPostById = (req, res) => {
     Post.findOne({ id })
         .then(post => {
             res.render('posts', { post });
-            return res.json({ success: true, post }).status(200);
+            // return res.json({ success: true, post }).status(200);
         })
         .catch(err => {
             console.log(err);
